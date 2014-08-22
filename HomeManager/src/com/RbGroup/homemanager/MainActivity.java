1e3961387bf2ff7150175ba7e264919d6dcac2e7
Yoonsung <lvev9925@naver.com>
2014-08-23 00:45:26 +0900

    Add Screen Sleep Prevent Code

--- HomeManager/src/com/RbGroup/homemanager/MainActivity.java
+++ HomeManager/src/com/RbGroup/homemanager/MainActivity.java
@@ -24,6 +24,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
@@ -89,6 +90,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
 		/*
 		 * Test Code
