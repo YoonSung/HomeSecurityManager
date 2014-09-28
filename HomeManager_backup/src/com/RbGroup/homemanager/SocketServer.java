feba7f2e638cda120ef30416c9db6709bf97ea78
Yoonsung <lvev9925@naver.com>
2014-09-28 16:29:17 +0900

    Rename to project

---
+++ HomeManager_backup/src/com/RbGroup/homemanager/SocketServer.java
@@ -0,0 +1,37 @@
+package com.RbGroup.homemanager;
+
+import java.net.InetSocketAddress;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import android.util.Log;
+
+public class SocketServer extends WebSocketServer{
+	public SocketServer(InetSocketAddress address) {
+		super(address);
+	}
+
+	@Override
+	public void onClose(WebSocket arg0, int arg1, String arg2, boolean arg3) {
+		Log.d("arduino", "close");
+	}
+
+	@Override
+	public void onError(WebSocket arg0, Exception e) {
+		Log.d("aurduino", "Error :" + e.toString());
+	}
+
+	@Override
+	public void onMessage(WebSocket arg0, String msg) {
+		Log.d("aurduino", "RCV :" + msg);
+
+	}
+
+	@Override
+	public void onOpen(WebSocket arg0, ClientHandshake arg1) {
+		Log.d("arduino", "websocket open..");
+
+	}
+}
