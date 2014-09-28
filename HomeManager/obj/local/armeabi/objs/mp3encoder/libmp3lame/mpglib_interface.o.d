1b91fb2005986c25d59388801e9ce40b37b9afd4
Yoonsung <lvev9925@naver.com>
2014-09-28 16:52:09 +0900

    Remove All Project To Problem Solve

--- HomeManager/obj/local/armeabi/objs/mp3encoder/libmp3lame/mpglib_interface.o.d
+++
@@ -1,2 +0,0 @@
-/Volumes/storage/project/Android/workspace/HomeManager/obj/local/armeabi/objs/mp3encoder/libmp3lame/mpglib_interface.o: \
- /Volumes/storage/project/Android/workspace/HomeManager/jni/libmp3lame/mpglib_interface.c
--- HomeManager/obj/local/x86/objs/stlport_static/__/gabi++/src/stdexcept.o.d
+++
@@ -1,2 +0,0 @@
-/Volumes/storage/project/Android/workspace/HomeManager/obj/local/x86/objs/stlport_static/__/gabi++/src/stdexcept.o: \
- /Volumes/storage/tool/android/sdk/ndk/sources/cxx-stl/stlport/../gabi++/src/stdexcept.cc
